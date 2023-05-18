95a3ffc12d0792734c89ac3682a5cae4d4f7771c
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-05-18 18:11:33 +0600

    firebaseConfig hidden using environment variable

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,14 +4,15 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCzZ-yOd5tDeF3kukVmBEl7LZW6uiUfuGo",
-  authDomain: "baby-zone-b21e1.firebaseapp.com",
-  projectId: "baby-zone-b21e1",
-  storageBucket: "baby-zone-b21e1.appspot.com",
-  messagingSenderId: "601465666835",
-  appId: "1:601465666835:web:9b00ad116698bc8cc9aee6",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
