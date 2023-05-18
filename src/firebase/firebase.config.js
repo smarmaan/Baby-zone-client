87644361cf43a23c155a8d2c80f10361ec69681a
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-05-18 17:30:46 +0600

    firebase installed... Config file added...

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCzZ-yOd5tDeF3kukVmBEl7LZW6uiUfuGo",
+  authDomain: "baby-zone-b21e1.firebaseapp.com",
+  projectId: "baby-zone-b21e1",
+  storageBucket: "baby-zone-b21e1.appspot.com",
+  messagingSenderId: "601465666835",
+  appId: "1:601465666835:web:9b00ad116698bc8cc9aee6",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
