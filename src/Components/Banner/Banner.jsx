66bf608a485cfc6942dd86b048fde65813462f05
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-05-19 15:21:39 +0600

    Banner carousel complete...

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,61 +1,51 @@
 import banner1 from "../../assets/Marvel-Legends.jpg";
 import banner2 from "../../assets/Marvel-Legends-2.jpg";
+import banner3 from "../../assets/Marvel-Legends-3.jpg";
+import banner4 from "../../assets/Marvel-Legends-4.png";
 
 const Banner = () => {
   return (
-    <div className="carousel w-full rounded-2xl">
+    <div className=" carousel w-full rounded-2xl shadow-2xl">
       <div id="slide1" className="carousel-item relative w-full">
-        <img
-          src={banner1}
-          className="w-full"
-        />
+        <img src={banner1} className="w-full" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide4" className="btn btn-circle">
+          <a href="#slide4" className="slides-btn-style">
             ❮
           </a>
-          <a href="#slide2" className="btn btn-circle">
+          <a href="#slide2" className="slides-btn-style">
             ❯
           </a>
         </div>
       </div>
       <div id="slide2" className="carousel-item relative w-full">
-        <img
-          src={banner2}
-          className="w-full"
-        />
+        <img src={banner2} className="w-full" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide1" className="btn btn-circle">
+          <a href="#slide1" className="slides-btn-style">
             ❮
           </a>
-          <a href="#slide3" className="btn btn-circle">
+          <a href="#slide3" className="slides-btn-style">
             ❯
           </a>
         </div>
       </div>
       <div id="slide3" className="carousel-item relative w-full">
-        <img
-          src={banner1}
-          className="w-full"
-        />
+        <img src={banner3} className="w-full" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide2" className="btn btn-circle">
+          <a href="#slide2" className="slides-btn-style">
             ❮
           </a>
-          <a href="#slide4" className="btn btn-circle">
+          <a href="#slide4" className="slides-btn-style">
             ❯
           </a>
         </div>
       </div>
       <div id="slide4" className="carousel-item relative w-full">
-        <img
-          src={banner2}
-          className="w-full"
-        />
+        <img src={banner4} className="w-full" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide3" className="btn btn-circle">
+          <a href="#slide3" className="slides-btn-style">
             ❮
           </a>
-          <a href="#slide1" className="btn btn-circle">
+          <a href="#slide1" className="slides-btn-style">
             ❯
           </a>
         </div>
--- src/Components/LeftCard/LeftCard.jsx
+++ src/Components/LeftCard/LeftCard.jsx
@@ -2,9 +2,9 @@ import cardImg from "../../assets/CardPicture.png";
 
 const LeftCard = () => {
   return (
-    <div className="card mx-auto h-full bg-green-200 shadow-xl">
-      <figure>
-        <img src={cardImg} alt="" />
+    <div className="card mx-5 md:mx-auto h-full bg-[#fbffdc] shadow-xl">
+      <figure className="m-auto ">
+        <img className="" src={cardImg} alt="" />
       </figure>
     </div>
   );
